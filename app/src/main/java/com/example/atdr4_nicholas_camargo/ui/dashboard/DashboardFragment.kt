6cda3faa2a320726c00490c5bd0b723a36f95772
NicholasGCamargo <nicholascamargo@gmail.com>
2020-06-09 19:30:00 -0300

    Fix visual e feita a tela de exibição dos files.

--- app/src/main/java/com/example/atdr4_nicholas_camargo/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/atdr4_nicholas_camargo/ui/dashboard/DashboardFragment.kt
@@ -1,11 +1,18 @@
 package com.example.atdr4_nicholas_camargo.ui.dashboard
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.atdr4_nicholas_camargo.R
+import com.example.atdr4_nicholas_camargo.adapter.AdapterGenericoAnotacao
+import com.example.atdr4_nicholas_camargo.adapter.ClasseAdapter
+import com.example.atdr4_nicholas_camargo.cripto.CriptografadorDeFiles
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+import java.io.File
 
 class DashboardFragment : Fragment() {
 
@@ -18,5 +25,47 @@ class DashboardFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
 
         return root
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        rcyVwListagemTotal.adapter = AdapterGenericoAnotacao(atualizarRecycle())
+        rcyVwListagemTotal.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+    }
+
+    fun atualizarRecycle(): List<ClasseAdapter> {
+        val location = File(requireContext().filesDir.toURI())
+        var prefix = ""
+        val dados = mutableListOf<ClasseAdapter>()
+        val files = location.listFiles()
+
+        files?.forEach {
+            if ("$prefix.txt" != it.name && "$prefix.fig" != it.name) {
+                prefix = it.name.removeSuffix(".txt")
+                prefix = prefix.removeSuffix(".fig")
+
+                dados.add(getDadosFile(prefix))
+            }
+        }
+
+        return dados
+    }
+
+    fun getDadosFile(prefix: String): ClasseAdapter {
+        var newPrefix = prefix.removeSuffix(".fig")
+        newPrefix = newPrefix.removeSuffix(".txt")
+        val imagem: ByteArray = CriptografadorDeFiles().lerImg("$newPrefix.fig", requireContext())
+        val text: String = CriptografadorDeFiles().lerFileTxt("$newPrefix.txt", requireContext())[2]
+
+        val bitmap = BitmapFactory.decodeByteArray(imagem, 0, imagem.size)
+
+        return ClasseAdapter(
+            prefix.split("(")[0],
+            text,
+            prefix.split("(")[1].removeSuffix(")"),
+            bitmap
+        )
     }
 }
